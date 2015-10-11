d24d243550813d88e065da9b7e3d24055c5d7ac1
manickyoj <hotshot354@gmail.com>
2015-10-11 16:28:32 -0400

    Cleanup of the target selector. Still much work to be done there.

--- client/react/components/NavDash/TargetSelector.jsx
+++ client/react/components/NavDash/TargetSelector.jsx
@@ -18,11 +18,27 @@ TargetSelector = React.createClass({
     const { physObjs, currentTarget, onTargetSelected } = this.props;
     const objsByType = PhysicsObjects.splitByType(physObjs);
 
+    const groupStyle = {
+      marginBottom: "10px",
+    }
+
+    const itemStyle = {
+      marginLeft: "10px",
+    }
+
     const targetLists = _.map(objsByType, (objs, type) => {
+      const items = objs.map((item, index) => {
+        return (
+          <div key={item.type + index} style={itemStyle}>
+              {item.name}
+          </div>
+        );
+      });
+
       return (
-        <div key={type}>
-          {type.toUpperCase()}:
-          {_.pluck(objs, 'name')}
+        <div key={type} style={groupStyle}>
+          <div>{type.toUpperCase()}:</div>
+          {items}
         </div>
       );
     });
